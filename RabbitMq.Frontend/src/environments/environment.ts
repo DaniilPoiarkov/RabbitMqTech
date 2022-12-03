12dda32e562f97c47f57bbe809b8fdc9f5495004
unknown <daniilpoyarkov@gmail.com>
2022-12-03 16:10:59 +0200

    Updated environment.ts file

--- RabbitMq.Frontend/src/environments/environment.ts
+++ RabbitMq.Frontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  webAPI: 'https://localhost:7036',
+  hubUrl: 'https://localhost:7036/notifications'
 };
 
 /*
