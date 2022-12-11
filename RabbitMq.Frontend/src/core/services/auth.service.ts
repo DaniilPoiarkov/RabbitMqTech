d0dbb24b16415ff4b1ec7f12ba71da4d68c52a0c
unknown <daniilpoyarkov@gmail.com>
2022-12-11 20:30:38 +0200

    Updated auth service

--- RabbitMq.Frontend/src/core/services/auth.service.ts
+++ RabbitMq.Frontend/src/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccessToken } from 'src/models/access-token';
 import { UserLogin } from 'src/models/user-login';
 import { UserRegister } from 'src/models/user-register';
 import { HttpService } from './http.service';
@@ -16,11 +17,11 @@ export class AuthService {
     private http: HttpService,
   ) { }
 
-  public login(userLogin: UserLogin): Observable<HttpResponse<string>> {
-    return this.http.putFullRequest(userLogin, this.baseUrl + '/login');
+  public login(userLogin: UserLogin): Observable<HttpResponse<AccessToken>> {
+    return this.http.putFullRequest(userLogin, this.baseUrl);
   }
 
-  public register(userRegister: UserRegister): Observable<HttpResponse<string>> {
+  public register(userRegister: UserRegister): Observable<HttpResponse<AccessToken>> {
     return this.http.postFullRequest(userRegister, this.baseUrl);
   }
 }
