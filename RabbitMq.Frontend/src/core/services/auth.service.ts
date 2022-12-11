fa54da7987572b3093aab5cbd925bafd5291180b
unknown <daniilpoyarkov@gmail.com>
2022-12-11 18:50:42 +0200

    implemented login and register requests in auth service

--- RabbitMq.Frontend/src/core/services/auth.service.ts
+++ RabbitMq.Frontend/src/core/services/auth.service.ts
@@ -1,4 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserLogin } from 'src/models/user-login';
+import { UserRegister } from 'src/models/user-register';
 import { HttpService } from './http.service';
 
 @Injectable({
@@ -6,11 +10,17 @@ import { HttpService } from './http.service';
 })
 export class AuthService {
 
+  baseUrl = '/api/auth';
+
   constructor(
     private http: HttpService,
   ) { }
 
-  public login(){
-    
+  public login(userLogin: UserLogin): Observable<HttpResponse<string>> {
+    return this.http.putFullRequest(userLogin, this.baseUrl + '/login');
+  }
+
+  public register(userRegister: UserRegister): Observable<HttpResponse<string>> {
+    return this.http.postFullRequest(userRegister, this.baseUrl);
   }
 }
