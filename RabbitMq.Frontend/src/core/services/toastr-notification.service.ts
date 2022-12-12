82f474709eb24ebf268671318bef6cccfe917cab
unknown <daniilpoyarkov@gmail.com>
2022-12-12 20:18:41 +0200

    Partial implemented wrapper for toastr notifications

---
+++ RabbitMq.Frontend/src/core/services/toastr-notification.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastrNotificationService {
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  public success(message: string, title?: string) : void {
+    this.toastr.success(message, title);
+  }
+
+  public error(message: string, title?: string) : void {
+    this.toastr.error(message, title);
+  }
+
+}
