213360ff231f56f6151455f369357eb91ffa6fc5
unknown <daniilpoyarkov@gmail.com>
2022-12-13 01:55:17 +0200

    Implemented warn and info toasts in npx-toastr wrapper and added ability to the service to set callbacks and change timeout of notification

--- RabbitMq.Frontend/src/app/auth/login/login.component.ts
+++ RabbitMq.Frontend/src/app/auth/login/login.component.ts
@@ -21,9 +21,6 @@ export class LoginComponent implements OnInit {
   public passwordControl: FormControl;
   public isErrorDisplay = false;
 
-  @ViewChild('passwordInput') passwordInput: InputComponent;
-  @ViewChild('emailInput') emailInput: InputComponent;
-
   constructor(
     private router: Router,
     private authService: AuthService,
@@ -78,7 +75,11 @@ export class LoginComponent implements OnInit {
     }
 
     if(!this.loginForm.valid) {
-      this.toastrService.error('Invalid values');
+      this.toastrService.error(
+        'Invalid values. Tap to reset fields', 
+        'Error', 
+        () => this.loginForm.reset());
+      
       this.loginForm.markAllAsTouched(); 
       this.isErrorDisplay = true;
       return;
@@ -101,7 +102,7 @@ export class LoginComponent implements OnInit {
       }
     }, (err) => {
       this.loginForm.reset();
-      this.toastrService.error(err.error.Error);
+      this.toastrService.error(err.error.Error, 'Error');
     });
   }
 
--- RabbitMq.Frontend/src/core/services/toastr-notification.service.ts
+++ RabbitMq.Frontend/src/core/services/toastr-notification.service.ts
@@ -10,12 +10,64 @@ export class ToastrNotificationService {
     private toastr: ToastrService
   ) { }
 
-  public success(message: string, title?: string) : void {
-    this.toastr.success(message, title);
+  public success(
+    message: string, 
+    title?: string, 
+    onTap?: () => void, 
+    ms: number = 5000) : void {
+
+    const toast = this.toastr.success(message, title, {
+      timeOut: ms,
+    });
+
+    if(onTap) {
+      toast.onTap.subscribe(() => onTap());
+    }
   }
 
-  public error(message: string, title?: string) : void {
-    this.toastr.error(message, title);
+  public error(
+    message: string, 
+    title?: string,
+    onTap?: () => void, 
+    ms: number = 5000) : void {
+
+    const toast = this.toastr.error(message, title, {
+      timeOut: ms
+    });
+
+    if(onTap) {
+      toast.onTap.subscribe(() => onTap());
+    }
+  }
+
+  public info(
+    message: string, 
+    title?: string,
+    onTap?: () => void, 
+    ms: number = 5000) : void {
+
+    const toast = this.toastr.info(message, title, {
+      timeOut: ms
+    });
+
+    if(onTap) {
+      toast.onTap.subscribe(() => onTap());
+    }
+  }
+
+  public warning(
+    message: string, 
+    title?: string,
+    onTap?: () => void, 
+    ms: number = 5000) : void {
+
+    const toast = this.toastr.warning(message, title, {
+      timeOut: ms
+    });
+
+    if(onTap) {
+      toast.onTap.subscribe(() => onTap());
+    }
   }
 
 }
