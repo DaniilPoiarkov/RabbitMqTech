4a3ba49f5063f341434af58ee8082ba6247331ba
unknown <daniilpoyarkov@gmail.com>
2022-12-12 23:30:24 +0200

    Implemented user-service and changed putFullRequest signature

---
+++ RabbitMq.Frontend/src/core/services/user.service.ts
@@ -0,0 +1,38 @@
+import { HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/models/user';
+import { HttpService } from './http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseUrl = '/api/user';
+
+  constructor(
+    private http: HttpService
+  ) { }
+
+  public getCurrentUser(): Observable<HttpResponse<User>> {
+    return this.http.getFullRequest(this.baseUrl + '/current');
+  }
+
+  public getUserById(id: number): Observable<HttpResponse<User>> {
+    return this.http.getFullRequest(this.baseUrl + '?id=' + id);
+  }
+
+  public getUserByEmail(email: string): Observable<HttpResponse<User>> {
+    return this.http.getFullRequest(this.baseUrl + '?email=' + email);
+  }
+
+  public getAllUsers(): Observable<HttpResponse<User[]>> {
+    return this.http.getFullRequest(this.baseUrl + '/all');
+  }
+
+  public setConnectionId(id: string): Observable<HttpResponse<void>> {
+    return this.http.putFullRequest(this.baseUrl + '?connectionId=' + id, undefined);
+  }
+  
+}
