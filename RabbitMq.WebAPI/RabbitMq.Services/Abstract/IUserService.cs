1aa0f8ac7864e76f760dc83794d2658275a084f7
unknown <daniilpoyarkov@gmail.com>
2023-01-27 20:10:13 +0200

    Removed nested code

--- RabbitMq.WebAPI/RabbitMq.Services/Abstract/IUserService.cs
+++ RabbitMq.WebAPI/RabbitMq.Services/Abstract/IUserService.cs
@@ -1,13 +1,12 @@
 ﻿using RabbitMq.Common.DTOs;
 
-namespace RabbitMq.Services.Abstract
+namespace RabbitMq.Services.Abstract;
+
+public interface IUserService
 {
-    public interface IUserService
-    {
-        Task<UserDto> GetUserById(int id, CancellationToken cancellationToken = default);
-        Task<UserDto> GetUserByEmail(string email, CancellationToken cancellationToken = default);
-        Task SetConnectionId(string connectionId, int userId, CancellationToken cancellationToken = default);
-        Task<List<UserDto>> GetAllUsers(CancellationToken cancellationToken = default);
-        Task UpdateAvatar(int userId, string avatarUrl, CancellationToken cancellationToken = default);
-    }
+    Task<UserDto> GetUserById(int id, CancellationToken cancellationToken = default);
+    Task<UserDto> GetUserByEmail(string email, CancellationToken cancellationToken = default);
+    Task SetConnectionId(string connectionId, int userId, CancellationToken cancellationToken = default);
+    Task<List<UserDto>> GetAllUsers(CancellationToken cancellationToken = default);
+    Task UpdateAvatar(int userId, string avatarUrl, CancellationToken cancellationToken = default);
 }
